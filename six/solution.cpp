f368460ac91d9fd4e53f55239196edf06a039c86
Alexandros Angeli <alexandrosangeli@gmail.com>
2023-12-07 11:21:36 +0000

    Optimised Day 6

--- six/solution.cpp
+++ six/solution.cpp
@@ -1,10 +1,41 @@
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int min(long a, long b) {
     return (a < b) ? a : b;
+}
+
+int max(long a, long b) {
+    return (a >= b) ? a : b;
+}
+
+long calc_combs_fast(vector<long> times, vector<long> distances)
+{
+    int n = times.size();
+    vector<int> combs(n, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+
+        int a = 1;
+        int comb;
+        long b = -times[i];;
+        long c = distances[i];
+
+        double x1 = (-b + pow(pow(b, 2) - (4*a*c), 0.5)) * (0.5 * a);
+        double x2 = (-b - pow(pow(b, 2) - (4*a*c), 0.5)) * (0.5 * a);
+
+        comb = floor(max(x1, x2)) - ceil(min(x1, x2)) + 1;
+        combs[i] = comb;
+    }
+
+    long product = 1;
+    for (int i = 0; i < n; i++) { product *= combs[i]; }
+
+    return product;
 }
 
 long calc_combs(vector<long> times, vector<long> distances)
@@ -28,10 +59,7 @@ long calc_combs(vector<long> times, vector<long> distances)
     }
 
     long product = 1;
-    for (int i = 0; i < n; i++) 
-    {
-        product *= combs[i];
-    }
+    for (int i = 0; i < n; i++) { product *= combs[i]; }
 
     return product;
 }
@@ -44,12 +72,12 @@ int main()
     times = {46, 85, 75, 82};
     distances = {208, 1412, 1257, 1410};
 
-    cout << "The answer for Day 6 Part 1 is: " << calc_combs(times, distances) << endl;
+    cout << "The answer for Day 6 Part 1 is: " << calc_combs_fast(times, distances) << endl;
 
     times = {46857582};
     distances = {208141212571410};
 
-    cout << "The answer for Day 6 Part 2 is: " << calc_combs(times, distances) << endl;
+    cout << "The answer for Day 6 Part 2 is: " << calc_combs_fast(times, distances) << endl;
 
     return 0;
 }
